5397b8a158669eb588f63d365008cd0399d167f2
Kyle Crawford <kcrwfrd@gmail.com>
2016-09-27 10:42:41 -0400

    Update munki_do.py

--- files/munki_do.py
+++ files/munki_do.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # encoding: utf-8
 #
-# Copyright 2009-2012 Greg Neagle.
+# Copyright 2009-2016 Greg Neagle.
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -18,9 +18,8 @@
 munki_do
 """
 import optparse
+import subprocess
 import sys
-import tempfile
-import shutil
 
 from munkilib import FoundationPlist
 from munkilib import updatecheck
@@ -29,52 +28,46 @@ from munkilib import munkicommon
 
 p = optparse.OptionParser()
 p.add_option('--catalog', '-c', action="append",
-             help='Which catalog to consult. May be specified multiple times.')
+           help='Which catalog to consult. May be specified multiple times.')
 p.add_option('--install', '-i', action="append",
-             help='An item to install. May be specified multiple times.')
+           help='An item to install. May be specified multiple times.')
 p.add_option('--uninstall', '-u', action="append",
-             help='An item to uninstall. May be specified multiple times.')
+           help='An item to uninstall. May be specified multiple times.')
 p.add_option('--checkstate', action="append",
-             help='Check the state of an item. May be specified multiple times.')
+           help='Check the state of an item. May be specified multiple times.')
 
 options, arguments = p.parse_args()
 cataloglist = options.catalog or ['production']
 
 if options.checkstate:
-    updatecheck.MACHINE = munkicommon.getMachineFacts()
-    updatecheck.CONDITIONS = munkicommon.getConditions()
-    updatecheck.getCatalogs(cataloglist)
-    for check_item in options.checkstate:
-        installed_state = 'unknown'
-        item_pl = updatecheck.getItemDetail(check_item, cataloglist)
-        if item_pl:
-            if updatecheck.installedState(item_pl):
-                installed_state = "installed"
-                exit_code = 0
-            else:
-                installed_state = "not installed"
-                exit_code = 1
-        print("%s: %s") % (check_item, installed_state)
-        sys.exit(exit_code)
+   updatecheck.MACHINE = munkicommon.getMachineFacts()
+   updatecheck.CONDITIONS = munkicommon.getConditions()
+   updatecheck.getCatalogs(cataloglist)
+   for check_item in options.checkstate:
+       installed_state = 'unknown'
+       item_pl = updatecheck.getItemDetail(check_item, cataloglist)
+       if item_pl:
+           if updatecheck.installedState(item_pl):
+               installed_state = "installed"
+               exit_code = 0
+           else:
+               installed_state = "not installed"
+               exit_code = 1
+       print "%s: %s" % (check_item, installed_state)
+       exit(exit_code)
 
 if not options.install and not options.uninstall:
-    sys.exit()
+   exit()
 
-temp_dir = tempfile.mkdtemp()
-temp_plist = (temp_dir + "/localmanifest.plist")
 manifest = {}
 manifest['catalogs'] = cataloglist
 manifest['managed_installs'] = options.install or []
 manifest['managed_uninstalls'] = options.uninstall or []
-FoundationPlist.writePlist(manifest, temp_plist)
-updatecheckresult = updatecheck.check(
-    localmanifestpath=temp_plist)
-if updatecheckresult == 1:
-    need_to_restart = installer.run()
-    if need_to_restart:
-        print("Please restart immediately!")
-
-try:
-    shutil.rmtree(temp_dir)
-except OSError:
-    print("Could not remove temp directory")
+FoundationPlist.writePlist(manifest, '/tmp/munki_do_localmanifest')
+munkicommon.report['ManifestName'] = 'munki_do_localmanifest'
+updatecheckresult = updatecheck.check( 
+    localmanifestpath='/tmp/munki_do_localmanifest') 
+if updatecheckresult == 1: 
+  need_to_restart = installer.run() 
+  if need_to_restart: 
+    print "Please restart immediately!" 
